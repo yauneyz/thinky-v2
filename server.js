1c25980dc09f27b1290dce7f1f7b136a73a592c1
Zac Yauney <zac.yauney@gmail.com>
2021-06-12 20:46:12 -0600

    sessions

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); const cookieParser = require('cookie-parser');
-const session = require('express-session');
+const session = require('cookie-session');
 const authRouter = require('./server/routes/auth');
 const boardsRouter = require('./server/routes/boards');
 const path = require("path")
