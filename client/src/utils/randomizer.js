e3a58b6ba0489a2c8d016e3ec5c1d21a9f1bf20c
Zac Yauney <zac.yauney@gmail.com>
2021-06-24 09:48:03 -0600

    Random words instead of pokemon

--- client/src/utils/randomizer.js
+++ client/src/utils/randomizer.js
@@ -1,3 +1,4 @@
+import randomWord from "random-words";
 const bank = [
   "Bulbasaur",
   "Ivysaur",
@@ -813,11 +814,6 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function random() {
-  const index = getRandomInt(bank.length);
-  return bank[index];
-}
-
 export default function getRandoms() {
-  return [random(), random(), random()];
+  return randomWord(3);
 }
