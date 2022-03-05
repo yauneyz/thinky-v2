3c12e81b96c13a84526b738d743965bb3a2aa294
Zac Yauney <zac.yauney@gmail.com>
2022-03-04 20:45:32 -0700

    Begin front end update

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,27 @@ import { connect } from "react-redux";
 import "./App.css";
 
 function App() {
-  return <h1>This is the app</h1>;
+  return (
+    <div className="wrapper">
+      <div className="title-bar"></div>
+      <div className="node-bar">
+        <ul className="node-list">
+          <li className="node-item">Boards</li>
+          <li className="node-item">VR Fitness Game</li>
+          <li className="node-item">Movement Patterns</li>
+          <li className="node-item">Fighting Patterns</li>
+        </ul>
+      </div>
+      <div className="wrapper2">
+        <div className="child-bar">
+          <ul className="child-list"></ul>
+        </div>
+        <div className="editor-box">
+          <textarea className="editor" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 const mapStateToProps = (_state) => {
