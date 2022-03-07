dca3b967c510552809d95100367146ba84167909
Zac Yauney <zac.yauney@gmail.com>
2022-03-07 16:54:37 -0700

    Front end progress

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,27 @@ import "./App.css";
 function App() {
   return (
     <div className="wrapper">
-      <div className="title-bar"></div>
+      <div className="title-bar">
+        <span className="title">Idea Editor</span>
+				<span className="wiki">Wiki</span>
+      </div>
       <div className="node-bar">
-        <ul className="node-list">
-          <li className="node-item">Boards</li>
-          <li className="node-item">VR Fitness Game</li>
-          <li className="node-item">Movement Patterns</li>
-          <li className="node-item">Fighting Patterns</li>
-        </ul>
+        <span className="node-item">Boards</span>
+        <span className="node-item">VR Fitness Game</span>
+        <span className="node-item">Movement Patterns</span>
+        <span className="node-item">Fighting Patterns</span>
       </div>
       <div className="wrapper2">
         <div className="child-bar">
-          <ul className="child-list"></ul>
+          <span className="child-node">Boxing</span>
+          <span className="child-node">Sword Fighting</span>
+          <span className="child-node">Skilled Projectiles</span>
+          <span className="child-node">Unskilled Projectiles</span>
+          <button className="add-child-button">Add</button>
         </div>
         <div className="editor-box">
+          <textarea className="editor" />
+          <textarea className="editor" />
           <textarea className="editor" />
         </div>
       </div>
