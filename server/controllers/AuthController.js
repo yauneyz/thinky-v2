c3f65e4a5b0b2e375c743fc92def577c53e3720b
Zac Yauney <zac.yauney@gmail.com>
2022-06-30 21:07:18 -0600

    Fixed registration

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,3 +1,41 @@
+const User = require("../models/UserModel");
+
+exports.register = (req, res) => {
+  const { email, uid } = req.body;
+  User.findOne({ email }, (err, foundUser) => {
+    if (err) {
+      res.status(500).send(err);
+    } else if (foundUser) {
+      res.status(400).send("User already exists");
+    } else {
+      const newBoard = {
+        name: "Main",
+        text: "",
+        expanded: true,
+        children: [],
+      };
+      console.log("UID", uid);
+      const newUser = {
+        email,
+        uid,
+        boards: newBoard,
+        open: 0,
+        trail: [],
+        tabs: [],
+      };
+
+      const insertedUser = new User(newUser);
+      insertedUser.save((err, savedUser) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.status(200).send(savedUser);
+        }
+      });
+    }
+  });
+};
+
 exports.user = (req, res) => {
   let data = null;
   if (req.session.user != null) {
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,5 +4,6 @@ const AuthController = require("../controllers/AuthController");
 const router = express.Router();
 
 router.get("/user", AuthController.user);
+router.post("/register", AuthController.register);
 
 module.exports = router;
